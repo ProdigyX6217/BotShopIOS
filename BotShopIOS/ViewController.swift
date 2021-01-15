8c52c9ccdf16a4fd2c06bb60edf972471a622e03
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-01-15 07:45:39 -0700

    project init

---
+++ BotShopIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BotShopIOS
+//
+//  Created by Student Laptop_7/19_1 on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}
+
