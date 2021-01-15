f6e3dddb8832d9e0dc385075f3d17832f851f5bb
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-01-15 08:26:44 -0700

    Set up basic collection view

---
+++ BotShopIOS/NewItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewItemCell.swift
+//  BotShopIOS
+//
+//  Created by Student Laptop_7/19_1 on 1/15/21.
+//
+
+import Foundation
+import UIKit
+
+class NewItemCell: UICollectionViewCell {
+    
+}
