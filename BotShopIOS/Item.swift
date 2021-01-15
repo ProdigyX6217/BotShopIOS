8c9d60804afeb2f7488f41c432cfa3627bb5f891
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-01-15 09:58:30 -0700

    Users can see items for sale in collection view

---
+++ BotShopIOS/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  BotShopIOS
+//
+//  Created by Student Laptop_7/19_1 on 1/15/21.
+//
+
+import Foundation
+import UIKit
+
+struct Item{
+    var title: String
+    var image: UIImage
+}
